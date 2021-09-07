e15f5460f770fd0a1fd7c10dc5638edbe47ae15f
joboot <jordan.booth41900@gmail.com>
2021-09-07 10:30:08 -0500

    Cleaned up and finished inputs. No calculations coded yet.

--- break_even_calc.py
+++ break_even_calc.py
@@ -12,7 +12,7 @@ print()
 # get the estimated revenue
 print("Estimate Revenue")
 print("-" * 39)
-crop_yield = float(input("Enter yield:\t"))
+crop_yield = input("Enter yield:\t")
 crop_price = float(input("Enter price:\t"))
 gov_payment = float(input("Enter government payment:\t"))
 print()
@@ -21,21 +21,7 @@ print()
 print("Estimate Revenue")
 print("-" * 39)
 variable_cost = float(input("Enter variable cost:\t"))
-overhead_cost = float(input("Enter overhead_cost:\t"))
-
-
-# get estimated cover crop needed
-print("Estimate Cover Crop needed")
-print("-" * 39)
-# get seeding rate from user
-seeding_rate = float(input("Enter seeding rate:\t"))
-
-# calculate cover crop needed
-cover_crop_needed = seeding_rate * acreage_area
-
-# print cover crop needed
-print()
-print("Total cover crop needed:", cover_crop_needed)
+overhead_cost = float(input("Enter overhead cost:\t"))
 
 print()
 print("Goodbye")
