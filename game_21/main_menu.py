56bfad31cb22e4864ee3f88df1c1f1d6c656f4b1
joboot <jordan.booth41900@gmail.com>
2021-12-07 10:09:42 -0600

    Added all the functions and documentation

--- game_21/main_menu.py
+++ game_21/main_menu.py
@@ -15,21 +15,37 @@ def main():
 
     :return:
     """
-    display_rules()
-    display_menu()
+    """
+    DICTIONARY SETUP
+    players = {
+        "Deb": {
+            "cash": 1.0,
+            "cards": [],
+            "cards_total": ,
+            "bet": 0.25
+        },
+        "Sam": {
+            "cash": 1.0,
+            "cards": [],
+            "cards_total": ,
+            "bet": 0.25        
+        }
+    }
+    """
+    display_intro()
 
 
-def display_rules():
+def display_intro():
     """
 
     :return:
     """
     utils.display_line()
-    print("\t\t   _________    __  _________   ___  ___")
-    print("\t\t  / ____/   |  /  |/  / ____/  |__ \<  /")
-    print("\t\t / / __/ /| | / /|_/ / __/     __/ // / ")
-    print("\t\t/ /_/ / ___ |/ /  / / /___    / __// /  ")
-    print("\t\t\____/_/  |_/_/  /_/_____/   /____/_/   ")
+    print("\t\t   _________    __  _________   ___  ___\n" +
+          "\t\t  / ____/   |  /  |/  / ____/  |__ \<  /\n" +
+          "\t\t / / __/ /| | / /|_/ / __/     __/ // / \n" +
+          "\t\t/ /_/ / ___ |/ /  / / /___    / __// /  \n" +
+          "\t\t\____/_/  |_/_/  /_/_____/   /____/_/   \n")
     utils.display_line()
 
     print("The rules are simple!\n" +
@@ -46,13 +62,100 @@ def display_rules():
     utils.display_line()
 
 
-def display_menu():
+def get_players():
     """
-
-    :return:
+    Get the player's names and then add the player to the dictionary setting up initial player's data
+    Until the user enters the word 'done' symbolizing no more players
+        set cash to $1.0
+        set cards to an empty list
+        set cards_total to 0
+        set bet to 0.25 default value
+    :return: n/a
     """
     print("Now lets get this game setup.  Who is all playing?\n" +
           "Please enter a player's name or enter 'done' when finished.")
+
+
+def play_round(players):
+    """
+    Call each function required to play one round
+        1) setup the new round
+        2) deal to each player
+        3) deal to the dealer
+        4) display the winners
+    :param players: 2D Dictionary of all player's data
+    :return: n/a
+    """
+
+
+def setup_new_round(players):
+    """
+    Reset all player's data for the next round.
+        set cards to an empty list
+        set cards_total to 0
+        set bet to 0
+    :param players: 2D dictionary of the player's data
+    :return:  n/a
+    """
+
+
+def deal_card(player_info):
+    """
+    Do the following three items:
+        Generate a random number between 1-10
+        add it to the card to the list of cards
+        add the card value to the cards total
+    :param player_info: one player's sub dictionary of cards, and cards_total
+    :return: n/a
+    """
+
+
+def deal_to_players(players):
+    """
+    Deal to all players using a for loop through the 2D dictionary
+        if the player is out of money then continue
+        else automatically deal them two cards
+        and then ask the player if they want another cards as long as they don't exceed 21
+        if they exceed 21 then continue to the next player
+        else ask them if they want to double their bet as long as they have at least 50 cents
+    :param players: 2D dictionary storing all player's data
+    :return: n/a
+    """
+
+
+def deal_to_dealer(players):
+    """
+    if there are no players still playing this hand (they did not exceed 21)
+    then the dealer automatically wins
+    otherwise start dealing to the dealer until he beats all players or reaches 21
+    :param players: 2D dictionary storing all player's data
+    :return: n/a
+    """
+
+
+def display_cards(cards):
+    """
+    Display one player's current list of cards on one line
+    :param cards: one player's current cards
+    :return: n/a
+    """
+
+
+def display_winners(players, dealer_cards_total):
+    """
+    Display the winners for the current round
+    :param players: 2D dictionary of all player's data
+    :param dealer_cards_total: the dealer's card total
+    :return: n/a
+    """
+
+
+def display_round_summary(players):
+    """
+    Display the round summary, how much money check player current has
+    :param players: 2D dictionary of all player's data
+    :return: the number of players who still has cash
+    """
 
 
 # runs this specific module's main
